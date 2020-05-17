54a52ef768c6e28f21209074db0467ccd1e63b74
Mo Elwekil <Mo@descod.net>
2020-05-17 21:59:19 +0100

    component ref added

--- src/component/Image.js
+++ src/component/Image.js
@@ -1,11 +1,13 @@
 import React from 'react'
+import ImageCard from './ImageCard';
 
 class Image extends React.Component{
+
     render(){
         return (
             <div className="images-block">
                 <ul>
-                    {this.props.allImages.map(({urls, id, alt_description}) => <li key={id}><img src={urls.regular} alt={alt_description} /></li>)}
+                    {this.props.allImages.map((image) => <li key={image.id}><ImageCard image={image} /></li>)}
                 </ul>
                 
             </div>
